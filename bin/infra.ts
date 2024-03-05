40a77607179c21f6c4f21132e66ad8b2e013c6b2
Bestnogm1 <koffiarielhessou@gmail.com>
2024-03-05 11:23:52 -0500

    added

--- bin/infra.ts
+++ bin/infra.ts
@@ -2,5 +2,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { InfraStack } from '../lib/infra-stack';
 
+
 const app = new cdk.App();
 new InfraStack(app, 'InfraStack');
+// new PipelineStack(app, "PipeLineStack")
--- lib/infra-stack.ts
+++ lib/infra-stack.ts
@@ -1,19 +1,37 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
+import * as s3Deployment from "aws-cdk-lib/aws-s3-deployment";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origin from "aws-cdk-lib/aws-cloudfront-origins";
 
+// https://aws-cdk.com/deploying-a-static-website-using-s3-and-cloudfront
 export class InfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'InfraQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    //create S3
+    const bucket = new s3.Bucket(this, 'testwebsites3', {
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true,
     });
 
-    const topic = new sns.Topic(this, 'InfraTopic');
+    // deployed Website inside of the s3
+    new s3Deployment.BucketDeployment(this, 'DeployWebsite', {
+      sources: [s3Deployment.Source.asset("../frontend")],
+      destinationBucket: bucket,
+      destinationKeyPrefix: 'web/static'
+    })
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    //granted read access to an S3 bucket (bucket) using the grantRead method.
+    const originAccessIdentity = new cloudfront.OriginAccessIdentity(this, 'OriginAccessIdentity');
+    bucket.grantRead(originAccessIdentity);
+
+    //A new CloudFront distribution is created with the name 'Distribution'. It's configured with the following options:
+    new cloudfront.Distribution(this, "distro", {
+      defaultBehavior: {
+        origin: new origin.S3Origin(bucket, { originAccessIdentity }),
+      }
+    })
   }
 }
